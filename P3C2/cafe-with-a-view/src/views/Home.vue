01259abc254beaea95b8a3048401eb9feaf60887
Marcelo Romero-Cors <luisromeroaraya@gmail.com>
2021-05-10 18:03:17 +0200

    Finished P3C2

---
+++ P3C2/cafe-with-a-view/src/views/Home.vue
@@ -0,0 +1,134 @@
+<template>
+  <div id="home" class="home">
+    <h1>{{ restaurantName }}</h1>
+    <p class="description">
+      Bienvenue dans notre café {{ restaurantName }}! Nous sommes réputés pour
+      notre pain et nos merveilleuses pâtisseries. Faites vous plaisir dès le
+      matin ou avec un goûter réconfortant. Mais attention, vous verrez qu'il
+      est difficile de s'arrêter.
+    </p>
+
+    <section class="menu">
+      <h2>Menu</h2>
+      <MenuItem
+        v-for="item in simpleMenu"
+        :key="item.name"
+        :name="item.name"
+        :image="item.image"
+        :price="item.price"
+        :quantity="item.quantity"
+        :inStock="item.inStock"
+        :addToShoppingCart="addToShoppingCart"
+      />
+    </section>
+
+    <aside class="shopping-cart">
+      <h2>Panier d'achat : {{ shoppingCart }} articles</h2>
+    </aside>
+    <footer class="footer">
+      <p>{{ copyright }}</p>
+    </footer>
+  </div>
+</template>
+
+<script>
+import MenuItem from "@/components/MenuItem.vue";
+
+export default {
+  name: "Home",
+  components: {
+    MenuItem,
+  },
+  data() {
+    return {
+      restaurantName: "La belle vue",
+      shoppingCart: 0,
+      simpleMenu: [
+        {
+          name: "Croissant",
+          image: {
+            source: "./images/croissant.jpg",
+            alt: "Un croissant",
+          },
+          inStock: true,
+          quantity: 1,
+          price: 2.99,
+        },
+        {
+          name: "Baguette de pain",
+          image: {
+            source: "./images/french-baguette.jpeg",
+            alt: "Quatre baguettes de pain",
+          },
+          inStock: true,
+          quantity: 1,
+          price: 3.99,
+        },
+        {
+          name: "Éclair",
+          image: {
+            source: "./images/eclair.jpg",
+            alt: "Éclair au chocolat",
+          },
+          inStock: false,
+          quantity: 1,
+          price: 4.99,
+        },
+      ],
+    };
+  },
+  computed: {
+    copyright() {
+      const currentYear = new Date().getFullYear();
+
+      return `Copyright ${this.restaurantName} ${currentYear}`;
+    },
+  },
+  methods: {
+    addToShoppingCart(amount) {
+      this.shoppingCart += amount;
+    },
+  },
+  beforeCreate() {
+    console.log("Je ne suis pas encore  créé");
+  },
+  created() {
+    console.log("Je suis créé !");
+  },
+  beforeMount() {
+    console.log("Je vais bientôt être monté sur le DOM!");
+  },
+  mounted() {
+    console.log("Je suis monté sur le DOM!");
+  },
+  beforeDestroy() {
+    console.log("Je suis sur le point de disparaître du DOM !");
+  },
+  destroyed() {
+    console.log("Je suis supprimé");
+  },
+};
+</script>
+
+<style>
+.description {
+  max-width: 960px;
+  font-size: 1.2rem;
+  margin: 0 auto;
+}
+.menu {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.shopping-cart {
+  position: absolute;
+  right: 30px;
+  top: 0;
+}
+.footer {
+  text-align: center;
+  font-style: italic;
+}
+</style>
